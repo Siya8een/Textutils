212680b5f0635d84dd1c7b66fe8c9b26aa3d1ac4
siya <siya@siyas-MacBook-Air.local>
2024-03-06 23:52:57 +0530

    About

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
     <>
-    ‹Navbar title="TextUtils" /›
+    <Navbar title="TextUtils" />
     <div className="container my-3">
-     <TextForm heading ="Enter the text to analyse below"/>
+    <TextForm heading ="Enter the text to analyse below"/>
+    <About/>
      </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,24 +7,49 @@ console.log ("Uppercase was clicked" + text);
 let newText = text. toUpperCase();
 setText (newText)
   }
+  const handleLoClick = ()=>{
+    console.log ("Lowercase was clicked" + text);
+    let newText = text. toLowerCase();
+    setText (newText)
+      }
+      const handleClearClick = ()=>{
+        console.log ("Lowercase was clicked" + text);
+        let newText = '';
+        setText (newText)
+          }
 const handleOnChange = (event)=>{
 console.log ("OnChange");
 setText(event.target.value);
 }
-  const [text, setText ] = useState('Enter text here');
+  const [text, setText ] = useState('');
 
   return (
-   <div>
+    <>
+   <div className="container">
     <h1>{props.heading}</h1>
 <div className="mb-3">
  
   <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
 </div>
-<button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+<button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+<button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
+<button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear text</button>
 </div> 
+
+<div className="container my-3 ">
+  <h3>Your Text Summary</h3>
+  <p>{text.split(" ").length} Words  {text.length} and Characters</p>
+  <p>{0.008 *text.split(" ").length} Minutes read </p>
+</div>
+</>
   )
 }
 
 
 // agar onchange ni hota to type nhi kr pata 
 //+text krke bo console mai show hone lga mtlb bo read krne lga input 
+
+
+
+// text analyzer
+// word count
